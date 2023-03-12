3685b36158d7ef0975c7301eb2a81424a9ca6a03
Alvin Haruna <harunaalvin69@gmail.com>
2023-03-12 18:56:17 +0100

    written out pseudo code for app

---
+++ app.py
@@ -0,0 +1,11 @@
+# create dictionary that stores questions and answers
+
+# declare a variable that tracks score of user
+
+# loop through dictionary
+
+# display each question to the user and allow them answer
+
+# show user if they are right or wrong
+
+# show final result when quiz is completed
