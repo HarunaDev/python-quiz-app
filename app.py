42617d86d7e1e4706fa28c3c0be9651b6151f1c5
Alvin Haruna <harunaalvin69@gmail.com>
2023-03-14 17:21:56 +0100

    added dictionary with questions

--- app.py
+++ app.py
@@ -1,4 +1,34 @@
 # create dictionary that stores questions and answers
+quiz = {
+    "question_1": {
+        "question": "What is the capital of France",
+        "answer": "Paris"
+    },
+    "question_2": {
+        "question": "What is the capital of Nigeria",
+        "answer": "Abuja"
+    },
+    "question_3": {
+        "question": "What is the capital of Germany",
+        "answer": "Berlin"
+    },
+    "question_4": {
+        "question": "What is the capital of Italy",
+        "answer": "Rome"
+    },
+    "question_5": {
+        "question": "What is the capital of Ghana",
+        "answer": "Accra"
+    },
+    "question_6": {
+        "question": "What is the capital of Togo",
+        "answer": "Lome"
+    },
+    "question_7": {
+        "question": "What is the capital of Japan",
+        "answer": "Tokyo"
+    },
+}
 
 # declare a variable that tracks score of user
 
